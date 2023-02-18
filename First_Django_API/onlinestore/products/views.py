39891dc5a552b014dbf309e21e40171ed035ab19
hassan-boulhilt <hboulhilt@gmail.com>
2023-02-18 04:45:50 +0100

    Work with JsonResponse class for api

--- First_Django_API/onlinestore/products/views.py
+++ First_Django_API/onlinestore/products/views.py
@@ -1,14 +1,44 @@
-from django.views.generic.detail import DetailView
-from django.views.generic.list import ListView
+from django.http import JsonResponse
+
+
+# from django.views.generic.detail import DetailView
+# from django.views.generic.list import ListView
 
 from .models import Product, Manufacturer
 
-class ProductDetailView(DetailView):
-    model = Product
-    template_name = "products/product_detail.html"
 
-class ProductListView(ListView):
-    model = Product
-    template_name = "products/product_list.html"
+def product_list(request):
+    products = Product.objects.all()
+    data = {"products": list(products.values())} #"pk", "name"
+    response = JsonResponse(data)
+    return response
+def product_detail(request, pk):
+    try:
+        product = Product.objects.get(pk=pk)
+
+        data = {"product":{"name": product.name,
+                            "description": product.description,
+                            "photo": product.photo.url,
+                            "price": product.price,
+                            "quantity": product.quantity,
+                            "shipping": product.shipping(),
+                            "total cost": product.totalcost(),
+                        }
+            }
+        response = JsonResponse(data)
+    except:
+
+        response =  JsonResponse({"error":{
+            "code": 404,
+            "message": "Product not found!"
+        }},status= 404)
+    return response
+# class ProductDetailView(DetailView):
+#     model = Product
+#     template_name = "products/product_detail.html"
+
+# class ProductListView(ListView):
+#     model = Product
+#     template_name = "products/product_list.html"
 
 
